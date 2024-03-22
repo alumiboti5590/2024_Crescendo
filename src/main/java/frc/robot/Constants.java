cef3662ae690d0e371db809e2e071c726059baa7
Alumiboti Laptop <robotics@sjci.com>
2024-03-21 20:02:36 -0400

    competition code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,11 +25,11 @@ public final class Constants {
     public static final class DriveConstants {
         // Driving Parameters - Note that these are not the maximum capable speeds of
         // the robot, rather the allowed maximum speeds
-        public static final double kMaxSpeedMetersPerSecond = 3.5;
+        public static final double kMaxSpeedMetersPerSecond = 4.5;
         public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
-        public static final double kDirectionSlewRate = 2; // radians per second
-        public static final double kMagnitudeSlewRate = 2; // percent per second (1 = 100%)
+        public static final double kDirectionSlewRate = 3; // radians per second
+        public static final double kMagnitudeSlewRate = 3; // percent per second (1 = 100%)
         public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
 
         // Chassis configuration
@@ -161,7 +161,7 @@ public final class Constants {
     public static final class OIConstants {
         public static final int kDriverControllerPort = 0;
         public static final int kOperatorControllerPort = 1;
-        public static final double kDriveDeadband = 0.2;
+        public static final double kDriveDeadband = 0.23;
     }
 
     public static final class AutoConstants {
